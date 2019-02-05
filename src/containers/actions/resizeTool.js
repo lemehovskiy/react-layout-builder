a3790b08d2370a8a47508f57549b6dbdb5e35925
lemehovskiy <lemehovskiy@gmail.com>
2019-02-05 15:43:50 +0200

    resize refactor

--- src/containers/actions/resizeTool.js
+++ src/containers/actions/resizeTool.js
@@ -1,37 +1,58 @@
+let progress = null,
+    changeValue = null,
+    returnVal = {};
+
+function getObjectResizeValuesSouth(mouse, objectInitState, mouseStartPosition) {
+    progress = mouse.y - mouseStartPosition.y;
+    changeValue = objectInitState.height + progress;
+    returnVal.y = changeValue > 0 ? objectInitState.y : objectInitState.y - Math.abs(changeValue);
+    returnVal.height = Math.abs(changeValue);
+
+    return returnVal;
+}
+
+function getObjectResizeValuesNorth(mouse, objectInitState, mouseStartPosition) {
+    progress = mouse.y - mouseStartPosition.y;
+        changeValue = objectInitState.height - progress;
+    returnVal.y = changeValue > 0 ? objectInitState.y - (changeValue - objectInitState.height) : objectInitState.y + objectInitState.height;
+    returnVal.height = Math.abs(changeValue);
+
+    return returnVal;
+}
+
+function getObjectResizeValuesEast(mouse, objectInitState, mouseStartPosition) {
+    progress = mouse.x - mouseStartPosition.x;
+    changeValue = objectInitState.width + progress;
+    returnVal.x = changeValue < 0 ? objectInitState.x + changeValue : objectInitState.x;
+    returnVal.width = Math.abs(changeValue);
+
+    return returnVal;
+}
+
+function getObjectResizeValuesWest(mouse, objectInitState, mouseStartPosition) {
+    progress = mouse.x - mouseStartPosition.x;
+    changeValue = objectInitState.width - progress;
+    returnVal.x = changeValue > 0 ? objectInitState.x + progress : objectInitState.x + objectInitState.width;
+    returnVal.width = Math.abs(changeValue);
+
+    return returnVal;
+}
+
 export const getObjectResizeValues = (mouse, direction, objectInitState, mouseStartPosition, object) => {
-    let updatedObject = {
-        x: object.x,
-        y: object.y,
-        width: object.width,
-        height: object.height
-    }
-    let progress = null,
-        changeValue = null;
+    let updatedObject = object;
 
     switch (direction) {
         case 's':
-            progress = mouse.y - mouseStartPosition.y;
-            changeValue = objectInitState.height + progress;
-            updatedObject.y = changeValue > 0 ? objectInitState.y : objectInitState.y - Math.abs(changeValue);
-            updatedObject.height = Math.abs(changeValue);
+            updatedObject = Object.assign(updatedObject, getObjectResizeValuesSouth(mouse, objectInitState, mouseStartPosition));
             break;
         case 'n':
-            progress = mouse.y - mouseStartPosition.y;
-            changeValue = objectInitState.height - progress;
-            updatedObject.y = changeValue > 0 ? objectInitState.y - (changeValue - objectInitState.height) : objectInitState.y + objectInitState.height;
-            updatedObject.height = Math.abs(changeValue);
+            updatedObject = Object.assign(updatedObject, getObjectResizeValuesNorth(mouse, objectInitState, mouseStartPosition));
             break;
         case 'e':
-            progress = mouse.x - mouseStartPosition.x;
-            changeValue = objectInitState.width + progress;
-            updatedObject.x = changeValue < 0 ? objectInitState.x + changeValue : objectInitState.x;
-            updatedObject.width = Math.abs(changeValue);
+            updatedObject = Object.assign(updatedObject, getObjectResizeValuesEast(mouse, objectInitState, mouseStartPosition));
             break;
         case 'w':
-            progress = mouse.x - mouseStartPosition.x;
-            changeValue = objectInitState.width - progress;
-            updatedObject.x = changeValue > 0 ? objectInitState.x + progress : objectInitState.x + objectInitState.width;
-            updatedObject.width = Math.abs(changeValue);
+            updatedObject = Object.assign(updatedObject, getObjectResizeValuesWest(mouse, objectInitState, mouseStartPosition));
             break;
     }
 
