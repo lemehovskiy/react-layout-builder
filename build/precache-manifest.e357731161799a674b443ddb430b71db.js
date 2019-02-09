766f65b9316b066becb3cccc6f1b8e075dceb0c8
lemehovskiy <lemehovskiy@gmail.com>
2019-02-09 13:53:47 +0200

    build

---
+++ build/precache-manifest.e357731161799a674b443ddb430b71db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70c438c279ab1ba210c9",
+    "url": "/static/js/main.70c438c2.chunk.js"
+  },
+  {
+    "revision": "f910efcd9a5da752b522",
+    "url": "/static/js/1.f910efcd.chunk.js"
+  },
+  {
+    "revision": "70c438c279ab1ba210c9",
+    "url": "/static/css/main.52dfa50a.chunk.css"
+  },
+  {
+    "revision": "f910efcd9a5da752b522",
+    "url": "/static/css/1.17143948.chunk.css"
+  },
+  {
+    "revision": "f2054b2d5e8ff7ee9f1d4a852fd4e886",
+    "url": "/index.html"
+  }
+];
