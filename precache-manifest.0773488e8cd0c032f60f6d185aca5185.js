f4dc9def6b4315e87e5753f5f2bdf40e669047e7
lemehovskiy <lemehovskiy@gmail.com>
2019-02-09 14:37:11 +0200

    Updates

---
+++ precache-manifest.0773488e8cd0c032f60f6d185aca5185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a621bb4fcab1ed8095",
+    "url": "/react-layout-builder/static/js/runtime~main.90a621bb.js"
+  },
+  {
+    "revision": "2b48f5b2b64c11522a8a",
+    "url": "/react-layout-builder/static/js/main.2b48f5b2.chunk.js"
+  },
+  {
+    "revision": "f910efcd9a5da752b522",
+    "url": "/react-layout-builder/static/js/1.f910efcd.chunk.js"
+  },
+  {
+    "revision": "2b48f5b2b64c11522a8a",
+    "url": "/react-layout-builder/static/css/main.52dfa50a.chunk.css"
+  },
+  {
+    "revision": "f910efcd9a5da752b522",
+    "url": "/react-layout-builder/static/css/1.17143948.chunk.css"
+  },
+  {
+    "revision": "c70d31c08e85c91088e0e999320d10e9",
+    "url": "/react-layout-builder/index.html"
+  }
+];
