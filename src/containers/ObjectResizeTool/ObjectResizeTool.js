22c47cb9ae4cdcec68de9b7ea37445e8f18dfaee
lemehovskiy <lemehovskiy@gmail.com>
2019-03-24 19:49:25 +0200

    hide frame when resize in progress

--- src/containers/ObjectResizeTool/ObjectResizeTool.js
+++ src/containers/ObjectResizeTool/ObjectResizeTool.js
@@ -7,12 +7,13 @@ const ObjectResizeTool = ({object, onMouseDown, resizeDirection}) => {
 
     return (
         <g {...getObjectAttributes(object)}>
-            <rect
+
+            {resizeDirection ? null : <rect
                 width={object.width}
                 height={object.height}
                 x={object.x}
                 y={object.y}
-                fill="none" stroke="#00a8ff" strokeDasharray="3 3" pointerEvents="none"/>
+                fill="none" stroke="#00a8ff" strokeDasharray="3 3" pointerEvents="none"/>}
 
             {dotsConfig.map(function (dot) {
                 return <circle key={dot.name} name={dot.name} cx={dot.x} cy={dot.y} fill="#34B7EF" r="5"
